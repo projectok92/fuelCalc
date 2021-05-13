135a0d7545ae18136e6bc79d902c89684b4c437f
Oleksandr Koval <oleksandrkoval@Oleksandrs-MacBook-Pro.local>
2021-05-13 13:48:59 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputs = document.getElementById('inputs').elements;
+
+const distanceValue = () => Math.abs(inputs['distance-input'].value);
+const priceValue = () => Math.abs(inputs['price-input'].value);
+const discountValue = () => Math.abs(inputs['discount-input'].value);
+const lphValue = () => Math.abs(inputs['lph-input'].value);
+
+
+const moneyToSomewhereAndBack = (dist = distanceValue(), price = priceValue(), discount = discountValue(), lph = lphValue()) => Math.floor(dist / 100 * lph * (price - discount));
+
+const printedResult = document.getElementById('printedResult');
+const result = () => printedResult.innerHTML = moneyToSomewhereAndBack() + ` грн`;
